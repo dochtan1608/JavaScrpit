0320908c39c6bb7641841fc4881104bd72fa68b9
dochtan1608 <89788071+dochtan1608@users.noreply.github.com>
2025-04-17 01:17:50 +0700

    assignment values and variables

---
+++ part1/assignments.js
@@ -0,0 +1,8 @@
+//PART 1
+//assignment values and variables
+let country = "Vietnam";
+let continent = "Asia";
+let population = 90000000;
+console.log(country);
+console.log(continent);
+console.log(population);
