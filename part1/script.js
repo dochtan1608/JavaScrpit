97954875bf8a06228866212fe03aa2827d543566
dochtan1608 <89788071+dochtan1608@users.noreply.github.com>
2025-04-17 01:08:34 +0700

    values and variables

--- part1/script.js
+++ part1/script.js
@@ -1,7 +1,30 @@
-let js = "okay";
-if (js === "awesome") {
-  alert("JavaScript is awesome!");
-} else {
-  alert("JavaScript is not awesome!");
-}
+let js = "amazing";
 console.log(40 + 8 + 23 - 10);
+
+console.log("Doan Chi Tan");
+console.log(23);
+
+let firstName = "Tan";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let chi_tan = "CT";
+let $function = 27;
+
+let person = "CTan";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Coder";
+
+let job1 = "programmer";
+let job2 = "coder";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+console.log(job1);
+console.log(job2);
+console.log(chi_tan);
+console.log($function);
+console.log("Num of PI = " + PI);
